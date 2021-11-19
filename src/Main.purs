b6d614a7c97d3fadea49da68bfbdfa1e66ca63fa
Vojta2 <vojtech.novotnyy@gmail.com>
2021-11-19 15:41:45 +0100

    POKUS PRVNÍ

--- src/Main.purs
+++ src/Main.purs
@@ -1,8 +1,8 @@
 module Main where
 
 import Prelude
-
-import Data.Grid (Coordinates)
+import Data.Array
+import Data.Grid (Coordinates, Grid, construct)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Reactor (Reactor, dimensions, executeDefaultBehavior, getW, runReactor, updateW_)
@@ -12,36 +12,58 @@ import Reactor.Graphics.Drawing (Drawing, fill, tile)
 import Reactor.Internal.Helpers (withJust)
 import Reactor.Reaction (Reaction)
 
-main :: Effect Unit
-main = runReactor reactor { title: "Moving Dot", width: 20, height: 20 }
 
-type World = { player :: Coordinates, cursor :: Maybe Coordinates }
+data Cell = Wall | Empty
+
+type World = {
+  board :: Grid Cell,
+  items :: Array Array Cell
+  player :: Coordinates
+}
+
+data MujEnum
+  = Up | Down | Right | Left
+
+width = 20
+height = 20
+
+main :: Effect Unit
+main = runReactor reactor { title: "Moving Dot", width, height}
 
 reactor :: Reactor World
 reactor = { initial, draw, handleEvent, isPaused: const true }
 
 initial :: World
-initial = { player: { x: 0, y: 0 }, cursor: Nothing }
+initial = let 
+  board = construct width height (\cords -> Empty)
+  items = replicate height $ replicate width Empty in 
+  {board, items, player: { x: 0, y: 0 }}
 
 draw :: World -> Drawing
-draw { cursor, player } = do
+draw { player } = do
   fill Color.blue400 $ tile player
-  withJust cursor $ \position ->
-    fill Color.gray200 $ tile position
 
 handleEvent :: Event -> Reaction World
-handleEvent event = do
-  { width, height } <- dimensions
-  let
-    clip a m = min (max 0 a) (m - 1)
-    bound { x, y } = { x: clip x width, y: clip y height }
-  { player: { x, y } } <- getW
+handleEvent event = 
   case event of
-    Mouse { position } -> updateW_ { cursor: Just position }
-
-    KeyPress { key: "ArrowLeft" } -> updateW_ { player: bound { x: x - 1, y } }
-    KeyPress { key: "ArrowRight" } -> updateW_ { player: bound { x: x + 1, y } }
-    KeyPress { key: "ArrowDown" } -> updateW_ { player: bound { x, y: y + 1 } }
-    KeyPress { key: "ArrowUp" } -> updateW_ { player: bound { x, y: y - 1 } }
+    KeyPress { key: "ArrowLeft" } -> mojeFunkce Left
+    KeyPress { key: "ArrowRight" } -> mojeFunkce Right
+    KeyPress { key: "ArrowDown" } -> mojeFunkce Down
+    KeyPress { key: "ArrowUp" } -> mojeFunkce Up
 
     _ -> executeDefaultBehavior
+  where
+    mojeFunkce mujEnum = do
+      { width, height } <- dimensions
+      let
+        clip a m = min (max 0 a) (m - 1)
+        bound { x, y } = { x: clip x width, y: clip y height }
+      { player: { x, y } } <- getW
+      case mujEnum of
+        Left -> updateW_ { player: bound { x: x - 1, y } }
+        Right -> updateW_ { player: bound { x: x + 1, y } }
+        Down -> updateW_ { player: bound { x, y: y + 1 } }
+        Up -> updateW_ { player: bound { x, y: y - 1 } }
+
+
+
